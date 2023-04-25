9beefc929b1ebf5bff4d7858180e04be2a36d3ec
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-04-25 10:07:59 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled10/index.js
+++ screens/Untitled10/index.js
@@ -13,9 +13,7 @@ const Untitled10 = () => {
       flex: 1
     }}><Pressable onPress={() => {
         navigation.navigate("ScreenAI11");
-      }}><Text style={styles.AsIDpirO}>My reviews</Text></Pressable><Pressable onPress={() => {
-        navigation.navigate("ScreenAI12");
-      }}><Text style={styles.StVqGNKI}>Notification</Text></Pressable><Text style={styles.UveiLiXo}>My Favorites</Text></ScrollView>
+      }}><Text style={styles.AsIDpirO}>My reviews</Text></Pressable><Pressable onPress={() => {}}><Text style={styles.StVqGNKI}>Notification</Text></Pressable><Pressable><Text style={styles.UveiLiXo}>My Favorites</Text></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
